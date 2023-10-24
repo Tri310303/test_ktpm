a357fc2f8cd5de357827e76397cf65dfae0d78f6
Tri Huynh 3103 <trihuynh3103@gmail.com>
2023-10-24 14:49:41 +0700

    main thay doi

--- thongbao.cpp
+++ thongbao.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main()
 {
 	cout << "Tri Huynh dang yeu qua troi luon aaaa" << endl;
-	cout << "Tri Huynh dang yeu qua troi luon aaaa123123" << endl;
+	int a=5; 
+	int b=1;
 	system("pause");
 	return 0;
 }
